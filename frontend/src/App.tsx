7796c151c84a8d6f1ea4590a68456701403c2017
Benny Li <59155001+Bennyli1995@users.noreply.github.com>
2024-01-26 11:34:50 -0800

    proper setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import * as React from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
